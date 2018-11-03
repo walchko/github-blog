543eed965ca4e706a6819ec04f9c904f028e14f8
walchko <walchko@users.noreply.github.com>
2018-11-03 10:54:49 -0600

    up

--- source/blog/Vision/stereo-vision/test.py
+++ source/blog/Vision/stereo-vision/test.py
@@ -3,9 +3,14 @@
 import cv2
 import time
 
+# windows 10
 # 0: black image
-# color image: 1 left (as you look at it)
+# usb2: color image: 1 left (as you look at it)
+
+# macos
 # usb3: (720, 2560, 3) 0
+# usb3: 1 is built-in camera
+# can't find depth camera
 camera = cv2.VideoCapture(0)
 
 cnt = 0
@@ -13,20 +18,22 @@ cnt = 0
 while True:
     ok, frame = camera.read()
     if ok:
-        print(frame.shape)
+        # print(frame.shape)
         h, w, d = frame.shape
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.resize(frame, (w//2,h//2))
-        print(frame.shape)
+        # frame = cv2.resize(frame, (w//2,h//2))
         # frame = cv2.equalizeHist(frame)
-        cv2.imshow("img", frame)
+        cv2.imshow("img", cv2.resize(frame, (w//3,h//3)))
+
         key = cv2.waitKey(1)
+
         if key == ord('q'):
             break
         elif key == ord('s'):
+            print(frame.shape)
             cv2.imwrite('image-{}.png'.format(cnt), frame)
             cnt += 1
-        time.sleep(1/10)
+        time.sleep(0.033)
     else:
         print(">> nothing: {}".format(ok))
         time.sleep(1)
