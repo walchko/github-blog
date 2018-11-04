a159be115fb9d35c771d12b14e67e6da59749a25
walchko <walchko@users.noreply.github.com>
2018-11-04 07:04:50 -0700

    up

--- source/blog/Vision/stereo-vision/camera_calibrate.py
+++ source/blog/Vision/stereo-vision/camera_calibrate.py
@@ -1,11 +1,104 @@
 #!/usr/bin/env python3
 import numpy as np
 import cv2
-import glob
-import argparse
+from glob import glob
+# import argparse
+from enum import Enum
+import time
+import pickle
 
 
-class StereoCalibration(object):
+Markers = Enum('Markers', 'checkerboard acircle')
+
+
+class Rectify(object):
+    def __init__(self, filename, alpha=0.5):
+        self.info = self.__read(filename)
+        self.alpha = alpha
+
+    def __read(self, filename, handler=pickle):
+        with open(filename, 'rb') as f:
+            data = handler.load(f)
+        # print(data)
+        return data
+
+    def __fix(self, image, m, d):
+        """
+        image: an image
+        alpha = 0: returns undistored image with minimum unwanted pixels (image
+                    pixels at corners/edges could be missing)
+        alpha = 1: retains all image pixels but there will be black to make up
+                    for warped image correction
+        """
+        h,w = image.shape[:2]
+        # Adjust the calibrations matrix
+        # alpha=0: returns undistored image with minimum unwanted pixels
+        #           (image pixels at corners/edges could be missing)
+        # alpha=1: retains all image pixels but there will be black to make
+        #           up for warped image correction
+        # returns new cal matrix and an ROI to crop out the black edges
+        newcameramtx, _ = cv2.getOptimalNewCameraMatrix(m, d, (w, h), self.alpha)
+        # undistort
+        ret = cv2.undistort(image, m, d, None, newcameramtx)
+        return ret
+
+    def undistort(self, image):
+        mtx = self.info['cameraMatrix']
+        dist = self.info['distCoeffs']
+        return self.__fix(image, mtx, dist)
+
+    def undistortLeft(self, image):
+        mtx = self.info['cameraMatrix1']
+        dist = self.info['distCoeffs1']
+        return self.__fix(image, mtx, dist)
+
+    def undistortRight(self, image):
+        mtx = self.info['cameraMatrix2']
+        dist = self.info['distCoeffs2']
+        return self.__fix(image, mtx, dist)
+
+    def undistortStereo(self, left, right):
+        return self.undistortLeft(left), self.undistortRight(right)
+
+
+class SCamera(object):
+    def imshow(self, imgs, scale=3, msec=500):
+        for i, img in enumerate(imgs):
+            h,w = img.shape[:2]
+            cv2.imshow('image-{}'.format(i), cv2.resize(img, (w//scale,h//scale)))
+            cv2.waitKey(msec)
+
+    def get_images(self, path):
+        """
+        Given a path, it reads all images. This uses glob to grab file names
+        and excepts wild cards *
+        Ex. cal.getImages('./images/*.jpg')
+        """
+        imgs_l = []
+        imgs_r = []
+        files = glob(path)
+
+        print("Found {} images at {}".format(len(tuple(files)), path))
+        # print('-'*40)
+
+        for i, f in enumerate(files):
+            img = cv2.imread(f, 0)
+            if img is None:
+                raise Exception('>> Could not read: {}'.format(f))
+            else:
+                if len(img.shape) > 2:
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                # print("[{}]:{} ({}, {})".format(i, f, *img.shape))
+                h, w = img.shape
+                l = img[:, :w//2]
+                r = img[:, w//2:]
+                imgs_l.append(l)
+                imgs_r.append(r)
+        # print('-'*40)
+        return imgs_l, imgs_r
+
+
+class CameraCalibration(object):
     def __init__(self):
         # termination criteria
         self.criteria = (cv2.TERM_CRITERIA_EPS +
@@ -13,86 +106,111 @@ class StereoCalibration(object):
         self.criteria_cal = (cv2.TERM_CRITERIA_EPS +
                              cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-5)
 
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        self.objp = np.zeros((9*6, 3), np.float32)
-        self.objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+    def save(self, filename, handler=pickle):
+        with open(filename, 'wb') as f:
+            handler.dump(self.info, f)
 
-        # Arrays to store object points and image points from all the images.
-        self.objpoints = []  # 3d point in real world space
-        self.imgpoints_l = []  # 2d points in image plane.
-        self.imgpoints_r = []  # 2d points in image plane.
+    def calibrate(self, images, marker_type, marker_size):
+         """
+         images: an array of grayscale images, all assumed to be the same size
+         """
+         self.marker_type = marker_type
+         self.marker_size = marker_size
+         self.save_cal_imgs = []
 
-        # self.cal_path = filepath
-        # self.read_images(self.cal_path)
+         # Arrays to store object points and image points from all the images.
+         objpoints = []  # 3d point in real world space
+         imgpoints = []  # 2d points in image plane.
 
-    # def read_images(self, cal_path):
-    #     images_right = glob.glob(cal_path + 'RIGHT/*.JPG')
-    #     images_left = glob.glob(cal_path + 'LEFT/*.JPG')
-    #     images_left.sort()
-    #     images_right.sort()
-    def cal(self, images):
-        for i, gray in enumerate(images):
-            # Find the chess board corners
-            ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+         max_corners = self.marker_size[0]*self.marker_size[1]
 
-            # If found, add object points, image points (after refining them)
-            self.objpoints.append(self.objp)
+         print("Images: {} @ {}".format(len(images), images[0].shape))
+         print("{} {}".format(marker_type, marker_size))
+         print('-'*40)
+         for cnt, gray in enumerate(images):
+             orig = gray.copy()
+             if len(gray.shape) > 2:
+                 gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
 
-            if ret is True:
-                rt = cv2.cornerSubPix(gray, corners, (11, 11),(-1, -1), self.criteria)
-                self.imgpoints.append(corners)
+             ret, objpoints, imgpoints, corners = self.findMarkers(gray, objpoints, imgpoints)
+             # If found, add object points, image points (after refining them)
+             if ret:
+                 print('[{}] + found {} of {} corners'.format(cnt, corners.size / 2, max_corners))
+                 term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.001)
+                 cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
 
-                # Draw and display the corners
-                # ret = cv2.drawChessboardCorners(img, (9, 6),corners, ret)
-                # cv2.imshow(images_left[i], img_l)
-                # cv2.waitKey(500)
+                 # Draw the corners
+                 tmp = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+                 cv2.drawChessboardCorners(tmp, self.marker_size, corners, True)
+                 self.save_cal_imgs.append(tmp)
+             else:
+                 print('[{}] - Could not find markers'.format(cnt))
 
-            img_shape = gray.shape[::-1]
+         # h, w = images[0].shape[:2]
+         # rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, (w, h), None, None)
 
-        rt, M1, d1, r1, t1 = cv2.calibrateCamera(objpoints, imgpoints, img_shape, None, None)
+         # images size here is backwards: w,h
+         rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+            objpoints, imgpoints, gray.shape[::-1], None, None)
+         print("RMS error: {}".format(rms))
+         print('-'*40)
 
-    def calibrate(self, images_left, images_right):
-        for i, gray_l, gray_r in enumerate(zip(images_left, images_right)):
-            # img_l = cv2.imread(images_left[i])
-            # img_r = cv2.imread(images_right[i])
+         self.data = {
+             'date': time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()),
+             'markerType': marker_type,
+             'markerSize': marker_size,
+             'imageSize': imgs_l[0].shape,
+             'cameraMatrix': mtx,
+             'distCoeffs': dist,
+             'rms': rms,
+             'rvecs': rvecs,
+             'tvecs': tvecs
+        }
+         # self.data = {'camera_matrix': mtx, 'dist_coeff': dist, 'rms': rms}
+         return rms, mtx, dist, rvecs, tvecs, objpoints, imgpoints
 
-            # gray_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
-            # gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+    def findMarkers(self, gray, objpoints, imgpoints):
+            # objp = np.zeros((self.marker_size[0]*self.marker_size[1],3), np.float32)
+            # objp[:,:2] = np.mgrid[0:self.marker_size[0],0:self.marker_size[1]].T.reshape(-1,2)
+            objp = np.zeros((np.prod(self.marker_size), 3), np.float32)
+            objp[:, :2] = np.indices(self.marker_size).T.reshape(-1, 2)  # make a grid of points
 
-            # Find the chess board corners
-            ret_l, corners_l = cv2.findChessboardCorners(gray_l, (9, 6), None)
-            ret_r, corners_r = cv2.findChessboardCorners(gray_r, (9, 6), None)
+            # Find the chess board corners or circle centers
+            if self.marker_type is Markers.checkerboard:
+                flags = 0
+                ret, corners = cv2.findChessboardCorners(gray, self.marker_size, flags=flags)
+            elif self.marker_type is Markers.acircle:
+                flags=cv2.CALIB_CB_ASYMMETRIC_GRID
+                ret, corners = cv2.findCirclesGrid(gray, self.marker_size, flags=flags)
+            else:
+                raise Exception("invalid marker type: {}".format(self.marker_type))
 
-            # If found, add object points, image points (after refining them)
-            self.objpoints.append(self.objp)
+            if ret:
+                # rt = cv2.cornerSubPix(gray_l, corners_l, (11, 11),(-1, -1), self.criteria)
+                imgpoints.append(corners.reshape(-1, 2))
+                objpoints.append(objp)
+            else:
+                corners = [] # didn't find any
 
-            if ret_l is True:
-                rt = cv2.cornerSubPix(gray_l, corners_l, (11, 11),(-1, -1), self.criteria)
-                self.imgpoints_l.append(corners_l)
+            return ret, objpoints, imgpoints, corners
 
-                # Draw and display the corners
-                ret_l = cv2.drawChessboardCorners(img_l, (9, 6),corners_l, ret_l)
-                cv2.imshow(images_left[i], img_l)
-                cv2.waitKey(500)
 
-            if ret_r is True:
-                rt = cv2.cornerSubPix(gray_r, corners_r, (11, 11),(-1, -1), self.criteria)
-                self.imgpoints_r.append(corners_r)
+class StereoCalibration(object):
+    def __init__(self):
+        self.camera_model = None
 
-                # Draw and display the corners
-                ret_r = cv2.drawChessboardCorners(img_r, (9, 6),corners_r, ret_r)
-                cv2.imshow(images_right[i], img_r)
-                cv2.waitKey(500)
-            img_shape = gray_l.shape[::-1]
+    def save(self, filename, handler=pickle):
+        if self.camera_model is None:
+            print("no camera model to save")
+            return
+        with open(filename, 'wb') as f:
+            handler.dump(self.camera_model, f)
 
-        rt, self.M1, self.d1, self.r1, self.t1 = cv2.calibrateCamera(
-            self.objpoints, self.imgpoints_l, img_shape, None, None)
-        rt, self.M2, self.d2, self.r2, self.t2 = cv2.calibrateCamera(
-            self.objpoints, self.imgpoints_r, img_shape, None, None)
+    def stereo_calibrate(self, imgs_l, imgs_r, marker_type, marker_size):
+        cc = CameraCalibration()
+        rms1, M1, d1, r1, t1, objpoints, imgpoints_l = cc.calibrate(imgs_l, marker_type, marker_size)
+        rms2, M2, d2, r2, t2, objpoints, imgpoints_r = cc.calibrate(imgs_r, marker_type, marker_size)
 
-        self.camera_model = self.stereo_calibrate(img_shape)
-
-    def stereo_calibrate(self, dims):
         flags = 0
         flags |= cv2.CALIB_FIX_INTRINSIC
         # flags |= cv2.CALIB_FIX_PRINCIPAL_POINT
@@ -108,11 +226,16 @@ class StereoCalibration(object):
 
         stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER +
                                 cv2.TERM_CRITERIA_EPS, 100, 1e-5)
+
         ret, M1, d1, M2, d2, R, T, E, F = cv2.stereoCalibrate(
-            self.objpoints, self.imgpoints_l,
-            self.imgpoints_r, self.M1, self.d1, self.M2,
-            self.d2, dims,
-            criteria=stereocalib_criteria, flags=flags)
+            objpoints,
+            imgpoints_l,
+            imgpoints_r,
+            M1, d1,
+            M2, d2,
+            imgs_l[0].shape[:2],
+            criteria=stereocalib_criteria,
+            flags=flags)
 
         print('Intrinsic_mtx_1', M1)
         print('dist_1', d1)
@@ -123,25 +246,55 @@ class StereoCalibration(object):
         print('E', E)
         print('F', F)
 
-        # for i in range(len(self.r1)):
+        # for i in range(len(r1)):
         #     print("--- pose[", i+1, "] ---")
-        #     self.ext1, _ = cv2.Rodrigues(self.r1[i])
-        #     self.ext2, _ = cv2.Rodrigues(self.r2[i])
-        #     print('Ext1', self.ext1)
-        #     print('Ext2', self.ext2)
+        #     ext1, _ = cv2.Rodrigues(r1[i])
+        #     ext2, _ = cv2.Rodrigues(r2[i])
+        #     print('Ext1', ext1)
+        #     print('Ext2', ext2)
 
-        print('')
+        # print('')
+        self.camera_model = {
+            'date': time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()),
+            'markerType': marker_type,
+            'markerSize': marker_size,
+            'imageSize': imgs_l[0].shape,
+            'cameraMatrix1': M1,
+            'cameraMatrix2': M2,
+            'distCoeffs1': d1,
+            'distCoeffs2': d2,
+            # 'rvecs1': r1,
+            # 'rvecs2': r2,
+            'R': R,
+            'T': T,
+            'E': E,
+            'F': F
+        }
 
-        camera_model = dict([('M1', M1), ('M2', M2), ('dist1', d1),
-                            ('dist2', d2), ('rvecs1', self.r1),
-                            ('rvecs2', self.r2), ('R', R), ('T', T),
-                            ('E', E), ('F', F)])
-
-        cv2.destroyAllWindows()
-        return camera_model
+        return ret
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', help='String Filepath')
-    args = parser.parse_args()
-    cal_data = StereoCalibration(args.filepath)
+
+    if False:
+        path = 'checkerboard-imgs/*.png'
+        marker = Markers.checkerboard
+        dims = (7,10)
+    else:
+        path = 'acircle-imgs/*.png'
+        marker = Markers.acircle
+        dims = (4,11)
+
+    scam = SCamera()
+    imgs_l, imgs_r = scam.get_images(path)
+    # scam.imshow(cc.save_cal_imgs, msec=1500)
+
+    sc = StereoCalibration()
+    ok = sc.stereo_calibrate(imgs_l, imgs_r, marker, dims)
+    if ok:
+        sc.save("camera_model.pickle")
+
+        rec = Rectify("camera_model.pickle", alpha=1)
+        cv2.imshow('image', rec.undistortLeft(imgs_l[3]))
+        cv2.waitKey()
+    else:
+        print("Crap ... failure")
