440def0d7ab30a5a879ca4fe2c65aa1451ca3094
walchko <walchko@users.noreply.github.com>
2019-03-03 21:11:14 -0500

    Update reinstall-jupyter.sh

--- reinstall-jupyter.sh
+++ reinstall-jupyter.sh
@@ -5,8 +5,8 @@
 PKGS="jupyter notebook nbformat nbconvert tornado widgetsnbextension ipywidgets \
 ipython-genutils ipython ipykernel jupyter-client jupyter-core jupyter-console"
 
-pip2 uninstall ${PKGS}
+# pip2 uninstall ${PKGS}
 pip3 uninstall ${PKGS}
 
-pip2 install -U jupyter
+# pip2 install -U jupyter
 pip3 install -U jupyter
